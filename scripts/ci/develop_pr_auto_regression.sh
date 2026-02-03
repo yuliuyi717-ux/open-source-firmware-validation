65a85fdb5a755bfc7f9224e89df9aa15c1ad93ab
Filip Gołaś <filip.golas@3mdeb.com>
2026-02-03 14:28:58 +0100

    add nucbox
    Signed-off-by: Filip Gołaś <filip.golas@3mdeb.com>

--- scripts/ci/develop_pr_auto_regression.sh
+++ scripts/ci/develop_pr_auto_regression.sh
@@ -76,8 +76,10 @@ for command in "${commands[@]}"; do
         trap snipeit_cleanup EXIT INT TERM HUP
         snipeit_checkout "$ASSET_ID" || exit 2
 
-        echo Run $i running basic-platform-setup
-        eval ./scripts/run.sh util/basic-platform-setup.robot > "$LOGS_DIR/run_${i}.log" 2>&1
+        if [ -n "$SKIP_BASIC_PLATFORM_SETUP" ]; then
+            echo Run $i running basic-platform-setup
+            eval ./scripts/run.sh util/basic-platform-setup.robot > "$LOGS_DIR/run_${i}.log" 2>&1
+        fi
         echo Run $i executing: \"${actual_command}\"
         eval ${actual_command} >> "$LOGS_DIR/run_${i}.log" 2>&1
     ) &
