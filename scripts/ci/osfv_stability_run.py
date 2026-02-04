5655c20cc338656df7df3f1c5aad46b92f2e907b
Filip Gołaś <filip.golas@3mdeb.com>
2026-02-04 15:00:15 +0100

    osfv_stability_run.py: accept env vars
    Signed-off-by: Filip Gołaś <filip.golas@3mdeb.com>

--- scripts/ci/osfv_stability_run.py
+++ scripts/ci/osfv_stability_run.py
@@ -12,23 +12,26 @@ import develop_pr_auto_regression
 import tqdm
 
 N_REPEATS = 5
-LOGS_DIR = "ci_logs"
-MANUAL_TESTS_LIST = "scripts/ci/regression-scope/configs/release_tests_suite_list.txt"
-DEVICES_LIST = "scripts/ci/regression-scope/configs/release_tests_devices.csv"
-RULES = "scripts/ci/regression-scope/configs/release_tests_rules.json"
-
 env = os.environ
+if "MANUAL_TESTS_LIST" not in env:
+    env["MANUAL_TESTS_LIST"] = (
+        "scripts/ci/regression-scope/configs/release_tests_suite_list_minimal.txt"
+    )
+if "DEVICES" not in env:
+    env["DEVICES"] = "scripts/ci/regression-scope/configs/release_tests_devices.csv"
+if "RULES_FILE" not in env:
+    env["RULES_FILE"] = "scripts/ci/regression-scope/configs/release_tests_rules.json"
+if "LOGS_DIR" not in env:
+    env["LOGS_DIR"] = "ci_logs"
+RULES = "scripts/ci/regression-scope/configs/release_tests_rules.json"
 env["ALLOW_DIRTY"] = "1"
-env["MANUAL_TESTS_LIST"] = MANUAL_TESTS_LIST
-env["DEVICES"] = DEVICES_LIST
-env["RULES_FILE"] = RULES
 
-shutil.rmtree(LOGS_DIR, ignore_errors=True)
-os.makedirs(LOGS_DIR)
+shutil.rmtree(env["LOGS_DIR"], ignore_errors=True)
+os.makedirs(env["LOGS_DIR"])
 
 repeats = tqdm.tqdm(range(N_REPEATS), colour="green")
 for i in repeats:
-    logs_dir = f"{LOGS_DIR}/run{i}"
+    logs_dir = f"{env["LOGS_DIR"]}/run{i}"
     os.makedirs(logs_dir)
     env["LOGS_DIR"] = logs_dir
 
